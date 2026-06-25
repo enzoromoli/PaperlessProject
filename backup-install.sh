4e7f6914b58858cf7e00f47f22c815a61aa82763
enzoromoli <enzoromoli44@gmail.com>
2026-06-25 22:27:15 +0200

    * Fix backups

--- backup-install.sh
+++ backup-install.sh
@@ -5,22 +5,41 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+# Install packages
 apt update
 apt install -y rclone restic cron
 
+# Install zip
+apt install -y zip
+
+# Get RESTIC_PASSWORD env var
 source .env
 export RESTIC_PASSWORD
 
+# Delete existing files
+rm -rf /opt/restic-repository
+
+# Init restic local respository
 mkdir -p /opt/restic-repository
 restic init --repo /opt/restic-repository
 
-rclone config create backup-paperless drive --drive-root-folder-id 1Rvoyn96zZRJy8oMPaCB51RdwIFf23Cjy
-cp ./backup-run.sh /opt/backup-run.sh
-chmod +x /opt/backup-run.sh
+# Create rclone config linked to a google drive folder
+rclone config delete backup-paperless
+rclone config create backup-paperless drive
+rclone config update backup-paperless root_folder_id 1SBAVA7RNcij2BSWb1pgbrXSAOLTx-YAf
 
+# Move .env & backup-run.sh files where wanted
+cp ./backup-run.sh /opt/backup-run.sh
 cp ./.env /opt/.env
 
-echo "0 * * * * /opt/backup-run.sh" > /etc/cron.d/backup-paperless
+# Allow backup-run.sh execution
+chmod +x /opt/backup-run.sh
+
+# Add cron task configuration to execute backup-run.sh moved every hour
+echo "0 * * * * root /opt/backup-run.sh" > /etc/cron.d/backup-paperless
+
+# Allow reading of the file from everyone
 chmod 644 /etc/cron.d/backup-paperless
 
+# Start cron task
 service cron start
--- paperless-install.sh
+++ paperless-install.sh
@@ -76,42 +76,49 @@ systemctl enable paperless-autostart
 systemctl start paperless-autostart
 
 # Security System
+# Install iptables
 apt install -y iptables
 
+# Delete all rules on all chains then delete custom chains
 iptables -F
 iptables -X
 
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT # Connexion déja établie
-iptables -A INPUT -p tcp -i eth0 --dport 8000 -j ACCEPT # Port 8000 de paperless
-iptables -A INPUT -i lo -j ACCEPT # local
-iptables -A INPUT -p icmp -j ACCEPT # ping
-iptables -A INPUT -j DROP # bloque le reste
+# Accept all input packets if the connexion with the sender is already established
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
+# Accept all input packets on the physical network card on the port 8000 (Paperless)
+iptables -A INPUT -p tcp -i eth0 --dport 8000 -j ACCEPT
+# Accept all from local packets
+iptables -A INPUT -i lo -j ACCEPT
+# Accept all icmp pings
+iptables -A INPUT -p icmp -j ACCEPT
+# Refuse all others input packets
+iptables -A INPUT -j DROP
 
-iptables -A OUTPUT -p icmp -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT # ping
+# Accept all icmp pings & new, established & related connexion for output packets
+iptables -A OUTPUT -p icmp -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT
 
+# Save the new config & apply
 iptables-save
 
+# Install fail2ban
 apt install -y fail2ban
-systemctl start fail2ban
-systemctl enable fail2ban
 
-#temp
-
-apt install -y fail2ban
+# Define a fail2ban request regex filter to POST /api/token route
 cat > /etc/fail2ban/filter.d/paperless-auth.conf <<EOF
 [Definition]
 failregex = ^<HOST> .* "POST /api/token/ HTTP/." 401
 ignoreregex =
 EOF
 
-#trop de 404 depuis la même IP
+# Define a fail2ban request regex filter to all GET/POST/HEAD routes (ignore static files)
 cat > /etc/fail2ban/filter.d/paperless-enum.conf <<EOF
 [Definition]
 failregex = ^<HOST> . "(GET|POST|HEAD) .* HTTP/." 404
 ignoreregex = ^<HOST> . "(GET|POST|HEAD) /(static|favicon.ico).* HTTP/.*" 404
 EOF
 
-#Configuration
+# Add a fail2ban configuration
+# Fail2ban will use it to protect our server from bruteforce & web enumeration attacks
 cat > /etc/fail2ban/jail.d/paperless.conf <<EOF
 [paperless-auth]
 enabled   = true
@@ -134,4 +141,6 @@ bantime   = 600
 action    = iptables-multiport[name=paperless-enum, port="http,https"]
 EOF
 
+# Enable & start fail2ban on the system
 systemctl enable fail2ban
+systemctl start fail2ban
